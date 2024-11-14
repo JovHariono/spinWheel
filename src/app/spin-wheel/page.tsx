ff0849562244b35327c4b36ccb4d319f44ee88f5
JovHariono <128022304+JovHariono@users.noreply.github.com>
2024-11-14 16:04:12 +0700

    switch gambar & upload asset

--- src/app/spin-wheel/page.tsx
+++ src/app/spin-wheel/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "../page.module.css";
 import { Button, Form } from "react-bootstrap";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FormData } from "../type";
 
 export default function SpinWheel() {
@@ -12,6 +12,26 @@ export default function SpinWheel() {
     jumlahPemenang: "",
     hadiah: "",
   });
+
+  const [image, setImage] = useState("/next.svg")
+
+  useEffect(() => {
+    switch (formData.hadiah) {
+      case "1":
+        setImage("/assets/lambo.png")
+        break;
+      case "2":
+        setImage("/assets/avanza.png")
+        break;
+      case "3":
+        setImage("/assets/kerupuk.png")
+        break;
+    
+      default:
+        setImage("/next.svg")
+        break;
+    }
+  }, [formData.hadiah]);
 
   return (
     <div>
@@ -22,7 +42,7 @@ export default function SpinWheel() {
           <p>Pilihan Hadiah: {formData.hadiah}</p>
         </div>
         <div className={styles.container}>
-          <Image alt="" src="/next.svg" width={180} height={32} />
+          <Image alt="" src={image} width={180} height={32} />
 
           <Form.Control as="textarea" placeholder="Nama Pemenang" />
 
