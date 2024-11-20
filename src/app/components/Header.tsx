0710f296b3ce74f9452b97e81c5cde84a922969d
JovHariono <128022304+JovHariono@users.noreply.github.com>
2024-11-20 13:49:18 +0700

    revisi nama hadiah

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -68,20 +68,44 @@ const Header: React.FunctionComponent<HeaderProps> = ({
                   setFormData((prev) => ({ ...prev, hadiah: e.target.value }))
                 }
               >
-                <option key={0} value="">Open this select menu</option>
+                <option key={0} value="">
+                  Open this select menu
+                </option>
                 {/* <option key={1} value="Headphone">Headphone</option> */}
-                <option key={1} value="Tumbler">Tumbler</option>
-                <option key={2} value="Rice Cooker">Rice Cooker</option>
-                <option key={3} value="Kompor Listrik">Kompor Listrik</option>
-                <option key={4} value="Emas Antam 0.5gr">Emas Antam 0.5gr</option>
-                <option key={5} value="Air Fryer">Air Fryer</option>
+                <option key={1} value="Tumbler">
+                  Mini Grand Price - Tumbler
+                </option>
+                <option key={2} value="Rice Cooker">
+                  Mini Grand Price - Rice Cooker
+                </option>
+                <option key={3} value="Kompor Listrik">
+                  Mini Grand Price - Kompor Listrik
+                </option>
+                <option key={4} value="Emas Antam 0.5gr">
+                  Mini Grand Price - Emas Antam 0.5gr
+                </option>
+                <option key={5} value="Air Fryer">
+                  Mini Grand Price - Air Fryer
+                </option>
 
-                <option key={6} value="Air Cooler">Air Cooler</option>
-                <option key={7} value="Emas Antam 1gr">Emas Antam 1gr</option>
-                <option key={8} value="Sepeda Lipat">Sepeda Lipat</option>
-                <option key={9} value="TV 32inch">TV 32inch</option>
-                <option key={10} value="Mesin Cuci">Mesin Cuci</option>
-                <option key={111} value="Handphone">Handphone</option>
+                <option key={6} value="Air Cooler">
+                  Grand Price - Air Cooler
+                </option>
+                <option key={7} value="Emas Antam 1gr">
+                  Grand Price - Emas Antam 1gr
+                </option>
+                <option key={8} value="Sepeda Lipat">
+                  Grand Price - Sepeda Lipat
+                </option>
+                <option key={9} value="TV 32inch">
+                  Grand Price - TV 32inch
+                </option>
+                <option key={10} value="Mesin Cuci">
+                  Grand Price - Mesin Cuci
+                </option>
+                <option key={111} value="Handphone">
+                  Grand Price - Handphone
+                </option>
                 {/* <option key={12} value="Air Fryer">Air Fryer</option> */}
               </Form.Select>
             </Form.Group>
