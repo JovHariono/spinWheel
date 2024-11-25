562633d451bee99278d226840c4bac4544684232
Indrakusuma <kindrakusuma@deloitte.com>
2024-11-25 15:56:39 +0700

    button

--- src/app/spin-wheel/page.tsx
+++ src/app/spin-wheel/page.tsx
@@ -577,7 +577,7 @@ export default function SpinWheel() {
 
           {showBtn && (
             <Button variant="link" onClick={() => spin(1)}>
-              <Image alt="" src={componentSpin} width={300} />
+              <Image alt="" src={componentSpin} width={150} />
             </Button>
           )}
         </div>
